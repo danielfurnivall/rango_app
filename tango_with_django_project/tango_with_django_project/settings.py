d42312efd1b8ec09f88bd32b2489382134df9936
danielfurnivall <danielfurnivall@gmail.com>
2020-01-29 20:42:46 +0000

    Chapter 4 - 4.2 complete Important note - as this seems to be a later version of Django than the book, we need to use static instead of staticfiles in our template

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -11,14 +11,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-print(__file__)
-print(os.path.dirname(__file__))
-print(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -121,5 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 STATIC_URL = '/static/'
+
